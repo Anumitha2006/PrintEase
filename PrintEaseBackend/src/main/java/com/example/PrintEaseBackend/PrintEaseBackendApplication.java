72b6a322b69985f93f8d6903eb4ede6cc2a6193d
vishwa070206 <vishwanathang2006@gmail.com>
2026-07-23 22:04:00 +0530

    Intial commit

---
+++ PrintEaseBackend/src/main/java/com/example/PrintEaseBackend/PrintEaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrintEaseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintEaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintEaseBackendApplication.class, args);
+	}
+
+}
---
+++ PrintEaseBackend/src/test/java/com/example/PrintEaseBackend/PrintEaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrintEaseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintEaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
